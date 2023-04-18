4428c5fbc607996924858362a8b9827bb88125df
PRANJAL <95075442+pranjalrana01@users.noreply.github.com>
2023-04-18 21:21:18 +0530

    Add files via upload

---
+++ RECURSION/checkSorted.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+    
+    bool isSorted(int arr[],int size){
+        if(size==0 || size == 1){
+            return true;
+        }
+        if(arr[0]>arr[1]){
+            return false;
+        }
+        else{
+            bool remainingPart = isSorted(arr+1,size-1);
+            return remainingPart;
+        }
+    }
+int main(){
+    int arr[5]={1,2,6,4,5};
+    int size = 5;
+
+    bool ans = isSorted(arr,size);
+
+    if(ans){
+        cout<<"array is sorted "<<endl;
+    }
+    else{
+        cout<<"array is not sorted "<<endl;
+    }
+
+    return 0;
+}
