63ea818915466d8ecbcd62dbfc3ed01e23193f11
pranjalrana01 <pranjalrana474@gmail.com>
2023-04-27 21:33:44 +0530

    OOPS

---
+++ OOPS/Encapsulation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+ class Student{
+
+    private:
+        string name;
+        int age;
+        int height;
+
+        public:
+        int getAge(){
+            return this->age;
+        }
+ };
+ 
+int main(){
+    Student first;
+    cout<<"sab sahi chal raha hai"<<endl;
+    return 0;
+}
