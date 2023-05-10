df1986901a06e9fa6034fbf00072c326b312add5
pranjalrana01 <pranjalrana474@gmail.com>
2023-05-10 21:56:44 +0530

    Linked list

---
+++ LINKED LIST/mergeTwoSortedLL.cpp
@@ -0,0 +1,45 @@
+void solve(Node<int>* first, Node<int>* second){
+
+    Node<int>* curr1 = first;
+    Node<int>* next1 = curr1->next;
+    Node<int>* curr2 = second;
+    Node<int>* next2 = curr2->next;
+
+    while((next1 != NULL) && (curr2 != NULL)){
+        if((curr2 -> data >= curr1->data) && (curr2 -> data <= next1 -> data)){
+            curr1 -> next = curr2;
+            next2 = curr2 -> next;
+            curr2 -> next = next1;
+
+            curr1 = curr2;
+            curr2 = next2;
+        }
+        else{
+            //curr1 and next1 ko aage badhana pdega
+            curr1 = next1;
+            next1 = next1 -> next;
+            if(next1 == NULL){
+                curr1 -> next = curr2;
+                return first;
+            }
+        }
+    }
+    return first;
+}
+
+Node<int>* sortTwoLists(Node<int>* first, Node<int>* second){
+    
+    if(first == NULL){
+        return second;
+    }
+        if(second == NULL){
+            return first;
+        }
+
+        if(first -> data <= second -> data){
+           return solve(first,second);
+        }
+        else{
+           return solve(second,first);
+        }
+}
